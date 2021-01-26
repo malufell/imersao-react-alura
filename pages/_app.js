8305116299085a55642d2eae3aa3edfe4fd0a02e
Malu <malufell@hotmail.com>
2021-01-26 20:39:33 -0300

    implementa og:image

--- pages/_app.js
+++ pages/_app.js
@@ -36,11 +36,13 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
-        <meta property="og:title" content={db.bg} key="title" />
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://imersao-react-alura.malufell.vercel.app/"/>
+        <meta property="og:title" content="Quiz How I Met Your Mother"/>
+        <meta property="og:image" content={db.bg}/>
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        { /* eslint-disable-next-line react/jsx-props-no-spreading */ }
         <Component {...pageProps} />
       </ThemeProvider>
     </>
