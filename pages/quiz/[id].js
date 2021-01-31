3a3aeb87bdba9f3962d90e7f6a6176996ef9d6fc
Malu <malufell@hotmail.com>
2021-01-31 18:22:56 -0300

    aula 5

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -7,34 +7,33 @@ import QuizScreen from '../../src/screens/Quiz';
 export default function QuizDaGaleraPage({ dbExterno }) {
   return (
     <ThemeProvider theme={dbExterno.theme}>
-    <QuizScreen
-      externalQuestions={dbExterno.questions}
-      externalBg={dbExterno.bg}
-    />
+      <QuizScreen
+        externalQuestions={dbExterno.questions}
+        externalBg={dbExterno.bg}
+      />
     </ThemeProvider>
   );
 }
 
 export async function getServerSideProps(context) {
-    const [projectName, githubUser] = context.query.id.split('___');
+  const [projectName, githubUser] = context.query.id.split('___');
 
-    try {
-        const dbExterno = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
-        .then((respostaDoServer) => {
-            if(respostaDoServer.ok) {
-                return respostaDoServer.json();
-            }
-            throw new Error('Falha em pegar os dados')
-        })
-        .then((respostaConvertidaEmObjeto) => respostaConvertidaEmObjeto);
-
-        return {
-          props: {
-              dbExterno,
-            } 
+  try {
+    const dbExterno = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
+      .then((respostaDoServer) => {
+        if (respostaDoServer.ok) {
+          return respostaDoServer.json();
         }
-    } catch(err) {
-        throw new Error(err)
-    }
-}    
+        throw new Error('Falha em pegar os dados');
+      })
+      .then((respostaConvertidaEmObjeto) => respostaConvertidaEmObjeto);
 
+    return {
+      props: {
+        dbExterno,
+      },
+    };
+  } catch (err) {
+    throw new Error(err);
+  }
+}
